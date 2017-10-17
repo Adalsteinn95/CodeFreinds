892517a4a597c6da2bbf8437e2f09304e9a8fcfc
Adalsteinn95 <adalsteinn95@gmail.com>
2017-10-17 19:03:33 +0000

    react-form komið af stað

--- Project/src/main/webapp/src/components/signup_form.js
+++ Project/src/main/webapp/src/components/signup_form.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-redux';
+import { connect } from 'react-redux';
+import { createUser } from '../actions';
 
 class SignUp extends Component {
-
-  render() {
-    return (
+  renderField(field){
+    return(
       <div>
-        Hérna verður Signup
+        <label>{field.label}</label>
+        <input
+        type="text"
+        {...field.input}
+        />
       </div>
     );
   }
 
+  onSubmit(values){
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Username for user"
+          name="username"
+          type="text"
+          Component={this.renderField}
+        />
+        <Field
+          label="Password for User"
+          name="password"
+          type="password"
+          Component={this.renderField}
+        />
+      </form>
+    );
+  }
 }
 
-export default SignUp;
+function validate(values){
+  const errors = {};
+
+  //handle the errors here
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'Create new User'
+})(
+  connect(null,{ createUser })(SignUp));
