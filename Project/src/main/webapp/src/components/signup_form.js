beecb8b7837a2527bd8b478085c10327d47785fb
Adalsteinn95 <adalsteinn95@gmail.com>
2017-10-18 00:44:05 +0000

    setup á react komið

--- Project/src/main/webapp/src/components/signup_form.js
+++ Project/src/main/webapp/src/components/signup_form.js
@@ -1,58 +1,15 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-redux';
-import { connect } from 'react-redux';
-import { createUser } from '../actions';
 
-class SignUp extends Component {
-  renderField(field){
-    return(
-      <div>
-        <label>{field.label}</label>
-        <input
-        type="text"
-        {...field.input}
-        />
-      </div>
-    );
-  }
+class Signup extends Component {
 
-  onSubmit(values){
-    console.log(values);
-  }
+    render() {
+        return (
+            <div>
+              Signup
+            </div>
+        );
+    }
 
-  render() {
-    const { handleSubmit } = this.props;
-
-    return (
-      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-        <Field
-          label="Username for user"
-          name="username"
-          type="text"
-          Component={this.renderField}
-        />
-        <Field
-          label="Password for User"
-          name="password"
-          type="password"
-          Component={this.renderField}
-        />
-      </form>
-    );
-  }
 }
 
-function validate(values){
-  const errors = {};
-
-  //handle the errors here
-
-  return errors;
-}
-
-export default reduxForm({
-  validate,
-  form: 'Create new User'
-})(
-  connect(null,{ createUser })(SignUp));
+export default Signup;
