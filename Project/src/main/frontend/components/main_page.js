c688586f287ce35c59bf3f787bae302d4fffeade
Adalsteinn95 <adalsteinn95@gmail.com>
2017-10-25 12:34:46 +0000

    added css

---
+++ Project/src/main/frontend/components/main_page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Question from './question';
+
+class MainPage extends Component {
+
+  render() {
+    return (
+      <div>
+        <Question />
+      </div>
+    );
+  }
+
+}
+
+export default MainPage;
